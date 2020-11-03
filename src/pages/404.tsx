9e4b910b9498e5e1f5c2aef2f2d8957acd549060
Talvanes Ferreira de Sousa <talvanes.ferreira.sousa@gmail.com>
2020-11-03 16:45:59 -0300

    nTDO Next: 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+import Layout from "@/components/Layout";
+import SEO from "@/components/SEO";
+
+import styles from "@/styles/pages/error.module.css";
+
+export default function Error() {
+  return (
+    <>
+    <SEO
+      title="Ops, algo aconteceu aqui!"
+      home
+    />
+
+    <Layout>
+      <section className={styles.error}>
+        <div className={styles.error_container}>
+          <header className={styles.error_title}>
+            <h1>Ops, algo aconteceu aqui.</h1>
+            <p className={styles.error_subtitle}>Parece que essa página não existe.</p>
+          </header>
+
+          <p className={styles.error_description}>Se você estiver perdido, <Link href="/"><a className={styles.error_description_link}>clique aqui para recomeçar</a></Link>.</p>
+        </div>
+      </section>
+    </Layout>
+    </>
+  );
+}
